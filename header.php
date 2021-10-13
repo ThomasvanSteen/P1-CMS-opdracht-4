148b47efc1ae2c3abc44e866f3914bc774d6193d
Thomas van Steen <210742@student.glu.nl>
2021-10-13 11:28:38 +0200

    3.2

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
     <head>
         <title><?php bloginfo('name'); ?></title>
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"
+        
     </head>
     <body>
-    
+        
